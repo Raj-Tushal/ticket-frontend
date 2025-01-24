eb01f2c410d9ec0b6856651f9deecfd1a99c5530
Raj Tushal <157106787+Raj-Tushal@users.noreply.github.com>
2025-01-25 03:55:37 +0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8002/api", 
+  baseURL: "ticket-backend-production-f1ba.up.railway.app", 
 });
 
 API.interceptors.request.use((req) => {
